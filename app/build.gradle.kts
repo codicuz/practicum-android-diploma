e39d6f66a19411ca1c9640b838a0f03a3a578d8a
Roman Balan <rabalan83@gmail.com>
2026-02-24 23:38:21 +0300

    Понижаем до 17-й джавы

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,13 +28,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
         buildConfig = true
         compose = true
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
