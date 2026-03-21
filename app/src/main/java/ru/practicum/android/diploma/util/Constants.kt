3da269506b8c1dbf9cc5b473dc45825d1200f56a
ITRoRo <roman.roschin2014@gmail.com>
2026-03-22 00:42:00 +0300

    Вспомогательные функции

---
+++ app/src/main/java/ru/practicum/android/diploma/util/Constants.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.util
+
+object Constants {
+    const val SEARCH_DEBOUNCE_DELAY_MILLIS = 2000L
+    const val CLICK_DEBOUNCE_DELAY_MILLIS = 1000L
+
+    const val HTTP_OK = 200
+    const val HTTP_BAD_REQUEST = 400
+    const val HTTP_NOT_FOUND = 404
+    const val HTTP_INTERNAL_SERVER_ERROR = 500
+
+
+}
