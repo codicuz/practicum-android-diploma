01db5a9d6f6fa23a3dac3f997008061a864b1ec5
ITRoRo <roman.roschin2014@gmail.com>
2026-04-05 19:24:44 +0300

    Номер телефона

--- app/src/main/java/ru/practicum/android/diploma/data/repositories/VacancyRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/repositories/VacancyRepositoryImpl.kt
@@ -115,7 +115,7 @@ class VacancyRepositoryImpl(
             address = address?.fullAddress,
             contactName = contacts?.name,
             contactEmail = contacts?.email,
-            contactPhones = contacts?.phone?.map { it.toPhoneInfo() } ?: emptyList(),
+            contactPhones = contacts?.phones?.map { it.toPhoneInfo() } ?: emptyList(),
             skills = skills ?: emptyList(),
             url = url
         )
